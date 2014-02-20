d1a5fbf7ca146d11a6ab54c746590c5dd6656e66
Mike Adeleke <madeleke13@gmail.com>
2014-02-20 02:10:56 +0000

    added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Domi::Application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resource :dashboard, only: [:show]
   root "homes#show", via: :get
 end
